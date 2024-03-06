078982730b2d901d04d5b55a45a741e8270f9568
Cecilia Swahn Gleisner <cecilia@lifeistooshort.se>
2024-03-06 14:59:38 +0000

    Add jQuery .addClass

--- js/myscript.js
+++ js/myscript.js
@@ -1,3 +1,11 @@
 $(document).ready(function () {
-
+    $("h2").addClass("underline"); // underlines all <h2> elements
+    $("ul").addClass("border");
 });
+
+/**
+ * Any code with the curly brackets above won't load until the DOM
+ *  is fully loaded and the htnl is rendered. */
+
+/** using .css to create inline styles is not good practice. 
+ * Instead create a class that can be added to any element you want to */
